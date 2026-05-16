8ff2d0f0f3b9e84e41bbf16cb7a744a565e16afe
gleb94-droid <gleb2009@gmail.com>
2026-05-17 02:12:21 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -697,7 +697,7 @@ function AdminPage({ lang }) {
                     )}
                   </div>
                 );
-              })}
+              })})()}
             </div>
           )
         }
