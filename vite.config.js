d2c6a551338dc93940ab6c1a3f301eb70bd827f0
gleb94-droid <gleb2009@gmail.com>
2026-05-17 00:16:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,4 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: false,
-    target: 'esnext',
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: { '.js': 'jsx', '.jsx': 'jsx' },
-    },
-  },
 })
